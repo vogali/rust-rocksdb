25404d39900ba8a577fac29b9d618a2f8a83d801
follitude <follitude2017@gmail.com>
2017-06-21 18:41:50 +0800

    remove malloc

--- tests/test_table_properties_collector_factory.rs
+++ tests/test_table_properties_collector_factory.rs
@@ -11,10 +11,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rocksdb::{Writable, DB, TablePropertiesCollector,TablePropertiesCollectorFactory, Options};
-use tempdir::TempDir;
-use std::collections::HashMap;
+use rocksdb::{Writable, DB, TablePropertiesCollector, TablePropertiesCollectorFactory, Options};
 use rocksdb::crocksdb_ffi::EntryType;
+use std::collections::HashMap;
+use tempdir::TempDir;
 
 struct CollectorFactory {
     _cnt: u64,
@@ -25,36 +25,22 @@ struct Collector {
 }
 
 impl TablePropertiesCollector for Collector {
-    fn add_userkey(&mut self, key: &[u8], value: &[u8], entry_type: EntryType){
-        if key[0]==b'A'{
-            self.cnt=self.cnt+1;
+    fn add_userkey(&mut self, key: &[u8], _: &[u8], _: EntryType) {
+        if key[0] == b'A' {
+            self.cnt = self.cnt + 1;
         }
     }
     fn finish(&mut self) -> HashMap<String, String> {
         let mut user_properties: HashMap<String, String> = HashMap::new();
-        user_properties.insert(String::from("The number of keys started with A"), 
-            format!("{}", self.cnt));
-        user_properties 
+        user_properties.insert(String::from("The number of keys started with A"),
+                               format!("{}", self.cnt));
+        user_properties
     }
 }
 
-impl TablePropertiesCollectorFactory for CollectorFactory{
-    fn create_table_properties_collector(&mut self, context: u32) 
-                                        -> Box<TablePropertiesCollector>
-    {
-        Box::new(Collector{cnt:0})
-    }
-}
-
-impl Drop for Collector {
-    fn drop(&mut self) {
-        println!("Drop collector");
-    }
-}
-
-impl Drop for CollectorFactory {
-    fn drop(&mut self) {
-        println!("Drop collector factory");
+impl TablePropertiesCollectorFactory for CollectorFactory {
+    fn create_table_properties_collector(&mut self, _: u32) -> Box<TablePropertiesCollector> {
+        Box::new(Collector { cnt: 0 })
     }
 }
 
@@ -64,10 +50,7 @@ fn test_table_properties_collector_factory() {
 
     let path = TempDir::new("_rust_rocksdb_collectortest").expect("");
     let mut opts = Options::new();
-    opts.add_table_properties_collector_factory("test",
-                               Box::new(CollectorFactory {
-                                   _cnt: 0,
-                               }))
+    opts.add_table_properties_collector_factory("test", Box::new(CollectorFactory { _cnt: 0 }))
         .unwrap();
     opts.create_if_missing(true);
     let db = DB::open(opts, path.path().to_str().unwrap()).unwrap();
@@ -80,5 +63,5 @@ fn test_table_properties_collector_factory() {
     db.flush(true).unwrap();
     drop(db);
     println!("path is {:?}", path);
-    mem::forget(path);  
+    mem::forget(path);
 }
